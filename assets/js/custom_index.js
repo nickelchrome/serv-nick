30666583d370666797aa3862d07e1f71130bbf7a
nickelchrome <81409727+nickelchrome@users.noreply.github.com>
2021-05-04 09:53:42 +0100

    Update custom_index.js

--- assets/js/custom_index.js
+++ assets/js/custom_index.js
@@ -10,7 +10,7 @@ document.getElementById("form_contact").addEventListener("submit", function (e) 
 
     Email.send({
         SecureToken : "46d446dd-390e-40d8-b0b5-cc49dd4e3c53",
-        To : 'rolandgarcia75000@gmail.com',
+        To : 'nickelapp266@gmail.com',
         From : "leonbonou20@gmail.com",
         Subject : "Nickel Contact " + subject,
         Body : "  <strong>Les informations du contact</strong> <br><br><br>\n" +
